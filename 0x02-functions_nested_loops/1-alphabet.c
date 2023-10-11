08185a56e036cd2fdf4bc9fdfeaf859b3a83de2b
jay-mothata <joey.mothata@gmail.com>
2023-10-11 15:56:03 -0700

    A function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: Prints alphabets in lowercase using only '_putchar'
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
