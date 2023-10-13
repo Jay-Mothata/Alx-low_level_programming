297c9718ae70923215c8a6fdd6237bab2c8ece9e
jay-mothata <joey.mothata@gmail.com>
2023-10-13 04:39:11 -0700

    A unction that prints a triangle

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" Fizz Buzz");
 		}
 		else if (i == 1)
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 		else
 		{
@@ -38,3 +38,4 @@ int main(void)
 
 	return (0);
 }
+
