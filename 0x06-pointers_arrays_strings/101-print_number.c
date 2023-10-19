07cc308e1f004510e10acc882c3f510bf04940fd
jay-mothata <joey.mothata@gmail.com>
2023-10-19 14:08:04 -0700

    A function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,12 +6,30 @@
  */
 void print_number(int n)
 {
+	unsigned int m, last_digit;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n == INT_MIN)
+		{
+			m = INT_MAX;
+			last_digit = 8;
+			_putchar('1');
+		}
+		else
+		{
+			m = -n;
+			last_digit = 0;
+		}
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	else
+	{
+		m = n;
+		last_digit = 0;
+	}
+
+	if (m / 10)
+		print_number(m / 10);
+	_putchar((m % 10 - last_digit) + '0');
 }
