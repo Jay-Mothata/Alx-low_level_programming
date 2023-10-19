92734530b034ddaa20f7d9bb94f3268773f148ae
jay-mothata <joey.mothata@gmail.com>
2023-10-19 13:45:44 -0700

    A function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,32 +9,21 @@
  */
 char *rot13(char *s)
 {
-	int i = 0;
-	int j = 0;
-	char data1[100];
-	char datarot[100];
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMONPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		for (j = 0; j < 52; j++)
 		{
-			s[i] = s[i] + 13;
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
 		}
-		else if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
-		{
-			s[i] = s[i] - 13;
-		}
-		data1[j] = s[i];
-		i++;
-		j++;
 	}
-	data1[j] = '\0';
-
-	for (i = 0; data1[i] != '\0'; i++)
-	{
-		datarot[i] = data1[i];
-	}
-	datarot[i] = '\0';
-
-	return (datarot);
+	return (s);
 }
