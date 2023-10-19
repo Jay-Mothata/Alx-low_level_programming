907f44b557b311c68435dee2281fb8dc1072eda8
jay-mothata <joey.mothata@gmail.com>
2023-10-19 14:45:35 -0700

    A unction that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,12 +6,18 @@
  */
 void print_number(int n)
 {
+	unsigned int u;
+
+	u = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		u = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	if (u / 10 != 0)
+	{
+		print_number(u / 10);
+	}
+	_putchar((u % 10) + '0');
 }
