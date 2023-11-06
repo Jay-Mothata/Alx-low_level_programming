f97905ab5ec8551d8518caa225d50ac4d4395f44
jay-mothata <joey.mothata@gmail.com>
2023-11-06 13:10:44 -0800

    Main Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,17 @@
 #define DOG_H
 
 /**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+/**
  * struct dog - Information about a dog
  * @name: The dog's name
  * @age: The dog's age
