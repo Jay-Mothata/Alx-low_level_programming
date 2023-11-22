cace50f4b102d196af429ced46843422606cdc91
jay-mothata <joey.mothata@gmail.com>
2023-11-22 04:50:37 -0800

    A function that deletes the node at index of a linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,7 +4,7 @@
  * reverse_listint - Reverses a listint_t linked list.
  * @head: A pointer to the pointer to the head of the listint_t list.
  *
- * Return: A pointer to the first node of the reversed list.
+ * Return: A pointer to the first node of the reversed list
  */
 
 listint_t *reverse_listint(listint_t **head)
