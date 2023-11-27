227dc8935c71e8e50aa0e47d75c5ab7ebf3e28fe
jay-mothata <joey.mothata@gmail.com>
2023-11-27 13:51:01 -0800

    Main Header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int open_file(const char *filename, int flags, mode_t permissions);
+void close_file(int file_descriptor, const char *filename);
+void handle_error(int error_code, int file_descriptor, const char *filename);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
