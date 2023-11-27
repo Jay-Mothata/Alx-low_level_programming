c2ae56f256006cec7453e3b7a92dd92454a29033
jay-mothata <joey.mothata@gmail.com>
2023-11-27 15:04:20 -0800

    Main Header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <elf.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -14,5 +17,6 @@ int open_file(const char *filename, int flags, mode_t permissions);
 void close_file(int file_descriptor, const char *filename);
 void handle_error(int error_code, int file_descriptor, const char *filename);
 int main(int argc, char *argv[]);
+void display_elf_header(const char *filename);
 
 #endif /* MAIN_H */
