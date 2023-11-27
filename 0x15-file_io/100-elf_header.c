63b02e7775c38b70499773c2fe32e93b832558e8
jay-mothata <joey.mothata@gmail.com>
2023-11-27 15:16:15 -0800

    A program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -79,11 +79,13 @@ void display_elf_header(const char *filename)
 	printf("	Class:			      %s\n",
 	       elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
 	printf("	Data:			       %s\n",
-	       elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+			elf_header.e_ident[EI_DATA] == ELFDATA2LSB ?
+			"2's complement, little endian" : "2's complement, big endian");
 	printf("	Version:		      %d (current)\n",
 	       elf_header.e_ident[EI_VERSION]);
 	printf("	OS/ABI:		       %s\n",
-	       elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+			elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV ?
+			"UNIX - System V" : "Other");
 	printf("	ABI Version:	      %d\n",
 	       elf_header.e_ident[EI_ABIVERSION]);
 	printf("	Type:			      %s\n",
