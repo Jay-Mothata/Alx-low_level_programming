5ceab94883b094f8246cb89c0b8f530a8fa91a94
jay-mothata <joey.mothata@gmail.com>
2023-11-27 16:16:19 -0800

    Main Header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,8 +7,6 @@
 #include <fcntl.h>
 #include <string.h>
 #include <elf.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 /* Function Prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -20,19 +18,5 @@ int open_file(const char *filename, int flags, mode_t permissions);
 void close_file(int file_descriptor, const char *filename);
 void handle_error(int error_code, int file_descriptor, const char *filename);
 int main(int argc, char *argv[]);
-
-/* ELF_HEADER Helper Functions */
-void display_elf_header(const char *filename);
-void check_elf(unsigned char *e_iden;
-void check_elf(unsigned char *e_ident);
-void handle_class(unsigned char *e_ident);
-void handle_data(unsigned char *e_ident);
-void handle_version(unsigned char *e_ident);
-void handle_osabi(unsigned char *e_ident);
-void handle_abi(unsigned char *e_ident);
-void handle_type(unsigned int e_type, unsigned char *e_ident);
-void handle_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf_fd);
-void print_usage(void);
 
 #endif /* MAIN_H */
